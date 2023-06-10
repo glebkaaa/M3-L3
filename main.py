e2dcfc10ad6c7edaeb59ec4df9d09964a0339191
glebkaaa <glebguslakov6@gmail.com>
2023-06-11 03:29:33 +0600

    update main.py

--- main.py
+++ main.py
@@ -1,56 +1,57 @@
-#Импорт
+import ssl
+import smtplib
 from flask import Flask, render_template, request
 from info import subject, password, sender
 from email.mime.text import MIMEText
-import smtplib
-import ssl
 
 
 app = Flask(__name__)
 
+
 def result_calculate(size, lights, device):
-    #Переменные для энергозатратности приборов
     home_coef = 100
     light_coef = 0.04
-    devices_coef = 5   
-    return size * home_coef + lights * light_coef + device * devices_coef 
+    devices_coef = 5
+    return size * home_coef + lights * light_coef + device * devices_coef
 
-#Первая страница
+
 @app.route('/')
 def index():
     return render_template('index.html')
-#Вторая страница
+
+
 @app.route('/<size>')
 def lights(size):
     return render_template(
-                            'lights.html', 
-                            size=size
-                           )
+        'lights.html',
+        size=size
+    )
 
-#Третья страница
+
 @app.route('/<size>/<lights>')
 def electronics(size, lights):
     return render_template(
-                            'electronics.html',                           
-                            size = size, 
-                            lights = lights                           
-                           )
+        'electronics.html',
+        size=size,
+        lights=lights
+    )
 
-#Расчет
+
 @app.route('/<size>/<lights>/<device>')
 def end(size, lights, device):
-    return render_template('end.html', 
-                            result=result_calculate(int(size),
-                                                    int(lights), 
-                                                    int(device)
-                                                    )
-                        )
-#Форма
+    return render_template('end.html',
+                           result=result_calculate(int(size),
+                                                   int(lights),
+                                                   int(device)
+                                                   )
+                           )
+
+
 @app.route('/form')
 def form():
     return render_template('form.html')
 
-# отправка сообщения на почту
+
 def send_email(message):
     context = ssl.create_default_context()
     try:
@@ -63,7 +64,7 @@ def send_email(message):
     except Exception as e:
         return f'An error occurred! The error is "{e}"'
 
-#Результаты формы
+
 @app.route('/submit', methods=['POST'])
 def submit_form():
     global email
@@ -71,24 +72,22 @@ def submit_form():
     email = request.form['email']
     address = request.form['address']
     date = request.form['date']
-    # запись в txt файл
     with open('file.txt', 'w', encoding='utf-8') as f:
-        f.write('Ваше имя: ' + name + '\n' + 'Ваша электронная почта: ' + email + '\n' + 'Ваш адрес: ' + address + '\n' + 'Ваша дата: ' + date)
-    # функция отправки сообщения на почту
+        f.write('Ваше имя: ' + name + '\n' + 'Ваша электронная почта: ' +
+                email + '\n' + 'Ваш адрес: ' + address + '\n' + 'Ваша дата: ' + date)
+
     send_email(f'''
         Ваше имя: {name}
         Ваша электронная почта: {email}
         Ваш адрес: {address}
         Ваша дата: {date}
     ''')
-    # здесь вы можете сохранить данные или отправить их по электронной почте
-    return render_template('form_result.html', 
-                           #Помести переменные
+    return render_template('form_result.html',
                            name=name,
                            email=email,
                            address=address,
                            date=date
                            )
-    
+
 
 app.run(debug=True)
